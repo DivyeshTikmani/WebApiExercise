9a92392c3af4c0618af4c3d887fe98ffc74d676b
DivyeshTikmani <tikmanidivyesh@gmail.com>
2016-07-07 03:20:47 +0530

    New Test Web API App

---
+++ PartyInvites/Controllers/RsvpController.cs
@@ -0,0 +1,26 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PartyInvites.Controllers
+{
+    public class RsvpController : ApiController
+    {
+        public IEnumerable<GuestResponse> GetAttendees()
+        {
+            return Repository.Responses.Where(a => a.WillAttend == true);
+        }
+
+        public void Response(GuestResponse response)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.Add(response);
+            }
+        }
+    }
+}
